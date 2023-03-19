6a15bb77d7a944cf7da60e0057f5840d618f4def
Mykola Zaiarnyi <mykola.zaiarnyi@eleks.com>
2023-03-19 15:38:08 +0200

    Revert "Corrupt test"
    This reverts commit 3f7e8f215c98d02870df0037a6e005aa34b71aa7.

--- ToDoApi.API.Tests/Validators/CreateUserDtoValidatorTests.cs
+++ ToDoApi.API.Tests/Validators/CreateUserDtoValidatorTests.cs
@@ -7,7 +7,6 @@ namespace ToDoApi.API.Tests.Validators
     public class CreateUserDtoValidatorTests
     {
         [Theory]
-        [InlineData(null)]
         [InlineData("123")]
         [InlineData("abc")]
         [InlineData("...")]
